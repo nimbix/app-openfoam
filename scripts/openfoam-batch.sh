dc74eceb05cd2d020841a8f6602c1d85167b1f2c
jhegge <johnathan.hegge@nimbix.net>
2020-02-10 13:28:27 -0600

    re #2424
    add in batch options

--- scripts/openfoam-batch.sh
+++ scripts/openfoam-batch.sh
@@ -27,6 +27,63 @@
 # those of the authors and should not be interpreted as representing official
 # policies, either expressed or implied, of Nimbix, Inc.
 
+# This JARVICE batch processing script is an example of submitting multiple
+# OpenFOAM processing steps in one multi-node run with MPI.
+#
+#  Here, we make the optional steps to implement the dam break example
+#  https://cfd.direct/openfoam/user-guide/v7-dambreak/#x7-640002.3.12
+#
+
+# Source the JARVICE job environment variables
+[[ -r /etc/JARVICE/jobenv.sh ]] && source /etc/JARVICE/jobenv.sh
+
+# Wait for slaves...max of 60 seconds
+SLAVE_CHECK_TIMEOUT=60
+TOOLSDIR="/usr/local/JARVICE/tools/bin"
+${TOOLSDIR}/python_ssh_test ${SLAVE_CHECK_TIMEOUT}
+ERR=$?
+if [[ ${ERR} -gt 0 ]]; then
+  echo "One or more slaves failed to start" 1>&2
+  exit ${ERR}
+fi
+
+# start SSHd
+if [[ -x /usr/sbin/sshd ]]; then
+  sudo /usr/sbin/sshd-keygen && sudo /usr/sbin/sshd
+fi
+
+set -e
+
+# parse command line
+CASE="/data/openfoam7/run"
+MESHTYPE="blockMesh"
+SOLVER="laplacianFoam"
+
+while [[ -n "$1" ]]; do
+  case "$1" in
+  -case)
+    shift
+    CASE="$1"
+    ;;
+  -mesh)
+    MESHTYPE="true"
+    ;;
+  -meshtype)
+    shift
+    MESHTYPE="$1"
+    ;;
+  -solver)
+    shift
+    SOLVER="$1"
+    ;;
+  *)
+    echo "Invalid argument: $1" >&2
+    exit 1
+    ;;
+  esac
+  shift
+done
+
 # OpenFOAM config dir
 FOAMETC=/opt/openfoam7/etc
 
@@ -40,12 +97,22 @@ echo "cd /data/openfoam7/run" >> ~/.bashrc
 # create the working dir, the "run" dir where files go, matches to FOAM_RUN in env
 mkdir -p /data/openfoam7/run
 
-# select Case dir
+# select Case dir. strip file name off path
+CASE=$(dirname "$CASE")
+echo "Using OpenFOAM Case directory: $CASE"
+cd "$CASE"
 
 # decompose prepped Mesh option
 
-# run selected mesh
+# run selected mesh and log
+echo "Running selected mesh and logging to $CASE/case.log"
+$MESHTYPE | tee -a "$CASE"/case.log
+
+# set initial fields
+echo "Running setFields and logging to $CASE/case.log"
+setFields | tee -a "$CASE"/case.log
 
 # run solver on mesh with MPI
+$SOLVER | tee -a "$CASE"/case.log
 
 # post-process prep: reconstruct mesh
